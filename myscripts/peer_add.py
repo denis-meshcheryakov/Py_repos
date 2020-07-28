82e292d2548ce145e4c1b0a10ea2eaa859734fd6
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2020-07-28 14:59:56 -0500

    totaly done

--- myscripts/peer_add.py
+++ myscripts/peer_add.py
@@ -10,28 +10,28 @@ oct1 = int(ip[0])
 oct2 = int(ip[1])
 oct3 = int(ip[2])
 oct4 = 1 + (int(ip[3]))
+oct5 = 1 + (int(oct4))
 
 int_vl700_temp = '''
 interface Vlan-interface700
 ip address {0:<}.{1:<}.{2:<}.{3:<}  255.255.255.252
 '''
 
-print(int_vl700_temp.format(oct1, oct2, oct3, oct4))
+print(int_vl700_temp.format(oct1, oct2, oct3, oct5))
 
-oct5 = 1 + (int(oct4))
 
 bgp_tamplate = '''
 bgp 64567
 undo peer 10.0.0.1
-peer {0:<}.{1:<}.{2:<}.{oct5} as-number 65301
-peer {0:<}.{1:<}.{2:<}.{oct5} as-path-acl 50 export
-peer {0:<}.{1:<}.{2:<}.{oct5} route-policy rm-sp2-out export
-peer {0:<}.{1:<}.{2:<}.{oct5} route-policy rm-sp2-in import
-peer {0:<}.{1:<}.{2:<}.{oct5} advertise-community
-peer {0:<}.{1:<}.{2:<}.{oct5} advertise-ext-community
-peer {0:<}.{1:<}.{2:<}.{oct5} fake-as 65524
+peer {0:<}.{1:<}.{2:<}.{oct4} as-number 65301
+peer {0:<}.{1:<}.{2:<}.{oct4} as-path-acl 50 export
+peer {0:<}.{1:<}.{2:<}.{oct4} route-policy rm-sp2-out export
+peer {0:<}.{1:<}.{2:<}.{oct4} route-policy rm-sp2-in import
+peer {0:<}.{1:<}.{2:<}.{oct4} advertise-community
+peer {0:<}.{1:<}.{2:<}.{oct4} advertise-ext-community
+peer {0:<}.{1:<}.{2:<}.{oct4} fake-as 65524
 quit
 sa fo
 '''
 
-print(bgp_tamplate.format(oct1, oct2, oct3, oct5 = oct5))
+print(bgp_tamplate.format(oct1, oct2, oct3, oct4 = oct4))
