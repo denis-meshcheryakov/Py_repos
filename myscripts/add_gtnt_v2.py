24affa6f5f0a95d3f23b5756ec5b6b769290aa83
ktulhoo86 <48226470+ktulhoo86@users.noreply.github.com>
2020-09-22 18:22:03 +0300

    Update add_gtnt_v2.py

--- myscripts/add_gtnt_v2.py
+++ myscripts/add_gtnt_v2.py
@@ -18,64 +18,71 @@ pe_ip = [str(oct1), str(oct2), str(oct3), str(oct5)]
 pe_ip = '.'.join(pe_ip)
 
 src_file_930 = '/var/share/config/rt-{}.hp-msr930.txt'.format(number_of_mag)
-dest_file_930 = '/var/share/config/rt-{}.hp-msr930-gtnt.txt'.format(number_of_mag)
 src_file_880 = '/var/share/config/rt-{}.cisco-880.txt'.format(number_of_mag)
-dest_file_880 = '/var/share/config/rt-{}.cisco-880-gtnt.txt'.format(number_of_mag)
 src_file_2800 = '/var/share/config/rt-{}.cisco-2800.txt'.format(number_of_mag)
-dest_file_2800 = '/var/share/config/rt-{}.cisco-2800-gtnt.txt'.format(number_of_mag)
 
-with open(src_file_930) as src, open(dest_file_930, 'a') as dest:
-    lines = src.readlines()
-    for line in lines:
+result_lst_930 = []
+result_lst_880 = []
+result_lst_2800 = []
+
+#  made config for hp-msr930
+with open(src_file_930) as src: 
+    for line in src:
         if 'ip address 10.0.0.2' in line:
             line = line.replace('10.0.0.2', ce_ip)
-            dest.write(line)
+            result_lst_930.append(line)
         elif 'peer 10.0.0.1' in line and '64996' in line:
             line = "peer {} as-number 65301\n".format(pe_ip)
-            dest.write(line)
+            result_lst_930.append(line)
         elif 'peer 10.0.0.1' in line and '64995' in line:
             line = "peer {} fake-as 65524\n".format(pe_ip)
-            dest.write(line)
+            result_lst_930.append(line)
         elif 'peer 10.0.0.1' in line:
             line = line.replace('10.0.0.1', pe_ip)
-            dest.write(line)
+            result_lst_930.append(line)
         else:
-            dest.write(line)
-            
-with open(src_file_880) as src, open(dest_file_880, 'a') as dest:
-    lines = src.readlines()
-    for line in lines:
-        if 'ip address 10.0.0.2' in line:
-            line = line.replace('10.0.0.2', ce_ip)
-            dest.write(line)
-        elif 'neighbor 10.0.0.1' in line and '64996' in line:
-            line = "neighbor {} remote-as 65301\n".format(pe_ip)
-            dest.write(line)
-        elif 'neighbor 10.0.0.1' in line and '64995' in line:
-            line = "neighbor {} local-as 65524\n".format(pe_ip)
-            dest.write(line)
-        elif 'neighbor 10.0.0.1' in line:
-            line = line.replace('10.0.0.1', pe_ip)
-            dest.write(line)
-        else:
-            dest.write(line)
-            
-with open(src_file_2800) as src, open(dest_file_2800, 'a') as dest:
-    lines = src.readlines()
-    for line in lines:
-        if 'ip address 10.0.0.2' in line:
-            line = line.replace('10.0.0.2', ce_ip)
-            dest.write(line)
-        elif 'neighbor 10.0.0.1' in line and '64996' in line:
-            line = "neighbor {} remote-as 65301\n".format(pe_ip)
-            dest.write(line)
-        elif 'neighbor 10.0.0.1' in line and '64995' in line:
-            line = "neighbor {} local-as 65524\n".format(pe_ip)
-            dest.write(line)
-        elif 'neighbor 10.0.0.1' in line:
-            line = line.replace('10.0.0.1', pe_ip)
-            dest.write(line)
-        else:
-            dest.write(line)
+            result_lst_930.append(line)
+with open(src_file_930, 'w') as dest:
+    dest.writelines(result_lst_930)
 
-print('<<<<<<<<<<JOB DONE>>>>>>>>>>')
+#  made config for cisco-880
+with open(src_file_880) as src:
+    for line in src:
+        if 'ip address 10.0.0.2' in line:
+            line = line.replace('10.0.0.2', ce_ip)
+            result_lst_880.append(line)
+        elif 'neighbor 10.0.0.1' in line and '64996' in line:
+            line = "neighbor {} remote-as 65301\n".format(pe_ip)
+            result_lst_880.append(line)
+        elif 'neighbor 10.0.0.1' in line and '64995' in line:
+            line = "neighbor {} local-as 65524\n".format(pe_ip)
+            result_lst_880.append(line)
+        elif 'neighbor 10.0.0.1' in line:
+            line = line.replace('10.0.0.1', pe_ip)
+            result_lst_880.append(line)
+        else:
+            result_lst_880.append(line)
+with open(src_file_880, 'w') as dest:
+    dest.writelines(result_lst_880)
+    
+#  made config for cisco-2800
+with open(src_file_2800) as src:
+    for line in src:
+        if 'ip address 10.0.0.2' in line:
+            line = line.replace('10.0.0.2', ce_ip)
+            result_lst_2800.append(line)
+        elif 'neighbor 10.0.0.1' in line and '64996' in line:
+            line = "neighbor {} remote-as 65301\n".format(pe_ip)
+            result_lst_2800.append(line)
+        elif 'neighbor 10.0.0.1' in line and '64995' in line:
+            line = "neighbor {} local-as 65524\n".format(pe_ip)
+            result_lst_2800.append(line)
+        elif 'neighbor 10.0.0.1' in line:
+            line = line.replace('10.0.0.1', pe_ip)
+            result_lst_2800.append(line)
+        else:
+            result_lst_2800.append(line)
+with open(src_file_2800, 'w') as dest:
+    dest.writelines(result_lst_2800)
+
+print('<<<<<<<<<<CONFIGS WITH GTNT DONE>>>>>>>>>>')
